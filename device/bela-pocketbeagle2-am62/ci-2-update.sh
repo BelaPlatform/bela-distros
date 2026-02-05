b2a1c3fa8979c5c5d13999c599dabbbd84854f34
Giulio Moro <giuliomoro@yahoo.it>
2026-02-05 17:24:38 -0500

    bela

---
+++ device/bela-pocketbeagle2-am62/ci-2-update.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+. ci-version.sh
+
+server_base_dir="https://rcn-ee.net/rootfs"
+
+arch="arm64"
+size="8gb"
+
+grab_image () {
+	wget -c --directory-prefix=/tmp/ ${server_base_dir}/${server_dir}/${date}/${file_prefix}.bmap
+	if [ ! -f /tmp/${file_prefix}.bmap ] ; then
+		echo "Failure to get ${server_base_dir}/${server_dir}/${date}/${file_prefix}.bmap"
+		exit 2
+	fi
+	mv -v /tmp/${file_prefix}.bmap ../../bmap-temp/
+
+	wget -c --directory-prefix=/tmp/ ${server_base_dir}/${server_dir}/${date}/${file_prefix}.img.xz.yml.txt
+	if [ ! -f /tmp/${file_prefix}.img.xz.yml.txt ] ; then
+		echo "Failure to get ${server_base_dir}/${server_dir}/${date}/${file_prefix}.img.xz.yml.txt"
+		exit 2
+	fi
+	mv -v /tmp/${file_prefix}.img.xz.yml.txt ${ymlfile}.yml
+}
+
+size="8gb"
+kernel_version="v6.12"
+server_dir="debian-${arch}-13-base-${kernel_version}-ti"
+file_prefix="${device}-debian-${debian_stable}-base-${kernel_version}-${arch}-${date}-${size}"
+
+ymlfile="bela-stable" ; grab_image
+
---
+++ device/bela-pocketbeagle2-am62/ci-version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+device="pocketbeagle2"
+date="2026-02-05"
+debian_stable="12.13"
